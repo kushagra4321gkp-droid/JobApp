0aeb8cec2c42116e1e5b52e47ec56436235499d2
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-23 19:30:03 +0530

    Adding Home and AddJob Controller

---
+++ src/main/java/com/Kushagra/App/JobController.java
@@ -0,0 +1,19 @@
+package com.Kushagra.App;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/", "home"})
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("addjob")
+    public String addjob(){
+        return "addjob";
+    }
+
+}
